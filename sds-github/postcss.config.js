a96658fe8a5c3283b753423da3de21a79ef30f37
Soofmax <124178747+Soofmaax@users.noreply.github.com>
2025-09-02 23:42:16 +0200

    Delete sds-github directory

--- sds-github/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // Enable CSS nesting before Tailwind as per Tailwind docs
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
